3197d59e38d28ff4154a61ce722631cfad06b7a8
adarsh1722 <adarsh0004singh@gmail.com>
2022-01-03 23:13:13 +0530

    Time: 55 ms (5.00%), Space: 5.9 MB (25.77%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -5,13 +5,6 @@ public:
         if( n <= 0)
             return false;
         
-        if(n % 3 == 0)
-            return isPowerOfThree(n/3);
-        
-        if(n == 1) 
-            return true;
-        
-        return false;
-        
+        return 1162261467 % n == 0;
     }
 };
