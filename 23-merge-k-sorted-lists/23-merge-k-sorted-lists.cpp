eb7bc5e1abd83d97f35348085dbdf01679565633
adarsh1722 <adarsh0004singh@gmail.com>
2022-01-29 18:43:33 +0530

    Time: 232 ms (19.65%), Space: 13.5 MB (46.04%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.cpp
@@ -12,29 +12,29 @@ class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         
-        vector<int>ans;
-        for(int i = 0 ; i < lists.size() ; i++)
+       if(lists.empty()){
+           return NULL;
+       }
+       while(lists.size() > 1){
+           lists.push_back(mergeTwoLists(lists[0],  lists[1]));
+           lists.erase(lists.begin());
+           lists.erase(lists.begin());
+       }
+        return lists.front();
+       
+    }
+    ListNode* mergeTwoLists(ListNode*l1 , ListNode* l2){
+        if(l1 == nullptr) return l2;
+        if(l2 == nullptr) return l1;
+        if(l1->val <= l2->val)
         {
-            ListNode* t = lists[i];
-            while(t)
-            {
-                ans.push_back(t->val);
-                t = t->next;
-            }
+            l1->next = mergeTwoLists(l1->next , l2);
+            return l1;
         }
-        sort(ans.begin() , ans.end());
-        ListNode  dummy(0);
-        ListNode* nxt = &dummy;
-        
-        for(int i = 0 ; i < ans.size() ; i++)
+        else
         {
-            ListNode* temp = new ListNode(ans[i]);
-            nxt->next = temp;
-            nxt = temp;
+            l2->next = mergeTwoLists(l1 , l2->next);
+            return l2;
         }
-        
-        return dummy.next;
-        
-        
     }
 };
