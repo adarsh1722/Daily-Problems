b66744d2b912fd0c28b80808fbfe6f42b3c59d44
adarsh1722 <adarsh0004singh@gmail.com>
2022-02-25 09:08:34 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (12.89%) - LeetHub

---
+++ 50-powx-n/50-powx-n.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        double ans = 0;
+        ans = (double)pow(x , n);
+        return ans;
+          
+        
+    }
+};
