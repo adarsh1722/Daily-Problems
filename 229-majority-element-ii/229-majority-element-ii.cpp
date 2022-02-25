ca3aa05db4bacaad19615d176b469cba51080dd0
adarsh1722 <adarsh0004singh@gmail.com>
2022-02-25 14:14:33 +0530

    Time: 21 ms (41.16%), Space: 16 MB (13.12%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -2,29 +2,16 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         
-        int a , b , cnt1= 0  , cnt2  = 0, n = nums.size();
+        unordered_map<int , int>mp;
         
-        for(int ele : nums)
-        {
-            if(ele  == a) cnt1++;
-            else if(ele == b) cnt2++;
-            else if(cnt1 == 0) a = ele ,cnt1++;
-            else if(cnt2 == 0) b = ele ,cnt2++;
-            else cnt1-=1 , cnt2-=1;
+        for(auto x : nums){
+            mp[x]++;
         }
-        
         vector<int>ans;
-        cnt1 = cnt2 = 0;
         
-        for(auto x : nums)
-        {
-            if(x  == a) cnt1++;
-            else if(x == b) cnt2++;
+        for(auto x : mp){
+            if(x.second > (nums.size())/3) ans.push_back(x.first);
         }
-        
-        if(cnt1 > n/3)ans.push_back(a);
-        if(cnt2 > n/3)ans.push_back(b);
-        
         return ans;
         
     }
