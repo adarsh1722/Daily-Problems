762dc18b80bed8c1ea9c02151f36726fec6f10a3
adarsh1722 <adarsh0004singh@gmail.com>
2022-02-26 19:18:28 +0530

    Time: 59 ms (19.74%), Space: 12 MB (17.52%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -3,32 +3,23 @@ public:
     int lengthOfLongestSubstring(string str) {
         
         int n = str.size();
-        int res = 0; // result
+        int res = 0;
 
         for (int i = 0; i < n; i++) {
-
-            // Note : Default values in visited are false
+            
             vector<bool> visited(256);  
 
             for (int j = i; j < n; j++) {
-
-                // If current character is visited
-                // Break the loop
+                
                 if (visited[str[j]] == true)
                     break;
 
-                // Else update the result if
-                // this window is larger, and mark
-                // current character as visited.
                 else {
                     res = max(res, j - i + 1);
                     visited[str[j]] = true;
                 }
             }
-
-            // Remove the first character of previous
-            // window
-            // visited[str[i]] = false;
+           
         }
         return res;
 
