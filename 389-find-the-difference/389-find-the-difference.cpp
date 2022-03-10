2848df24dd7fe764968e8ef759a9bd6e5d6cdee5
adarsh1722 <adarsh0004singh@gmail.com>
2022-03-10 22:56:26 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (98.42%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -4,22 +4,17 @@ public:
         
         if(s.size() == 0){
             return t[0];
-        }
-       
-        
-        char res ;
-      
-        int i = 0;
-        res = s[0]^t[0];
-        i+=1;
-        while (i < s.size()){
-            res = res^s[i]^t[i];
-            i+=1;
             
+        }
+        
+        char res = s[0]^t[0];
+        int i = 1;
+        while(i < s.size()){
+            res = res^s[i]^t[i];
+            i += 1;
         }
         res = res^t[i];
         return res;
-        
         
     }
 };
