a6f744119e91fb941f8f10a5a1300a7afdbb59e8
adarsh1722 <adarsh0004singh@gmail.com>
2022-03-10 10:30:35 +0530

    Time: 39 ms (72.76%), Space: 71.4 MB (50.05%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -1,23 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
-        int carry = 0 ;
+        int carry = 0;
         ListNode newHead(0);
         ListNode* t = &newHead;
         
         while(carry || l1 || l2)
         {
-            carry += (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
+            carry += (l1 ? l1->val : 0) + ( l2 ? l2->val : 0);
             t->next = new ListNode(carry%10);
             t = t->next;
             carry /= 10;
-            if(l1) l1 = l1->next;
-            if(l2) l2 = l2->next;
+            if(l1)l1 = l1->next;
+            if(l2)l2 = l2->next;
         }
-        
         return newHead.next;
-        
         
         
     }
