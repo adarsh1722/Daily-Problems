da82a224ee0c59e5e35c807e5da6f4761e25a146
adarsh1722 <adarsh0004singh@gmail.com>
2022-03-21 23:08:04 +0530

    Time: 47 ms (25.39%), Space: 22.4 MB (55.94%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -2,17 +2,39 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         
-        unordered_map<int , int>mp;
-        for(auto  x : nums){
-            mp[x]++;
-        }
         
-        for(auto x : mp){
-            if(x.second == 1){
-                return x.first;
-                
-            }
+        int low = 0 , high = nums.size()-1;
+        int mid = 0;
+        
+        // when there is a single element
+        if(high == 0) return nums[0];
+        // when first element is the non repeating element
+        else if(nums[0] != nums[1]) return nums[0];
+        // when last element is the non repeating element
+        else if(nums[high] != nums[high-1]) return nums[high];
+        
+        // other conditions
+        while(low <= high){
+            
+            // calculate mid
+            mid = low + (high - low) / 2;
+            
+            // unique element property
+            if(nums[mid] != nums[mid + 1] && nums[mid] != nums[mid-1] )
+                return nums[mid];
+            
+            if((mid %2 == 0 && nums[mid] == nums[mid + 1]) || (mid%2 == 1 && nums[mid] == nums[mid-1]) ) 
+                low = mid + 1;
+            else
+                high = mid - 1;
+            
+                   
+            
+            
+            
         }
         return -1;
+        
+        
     }
 };
