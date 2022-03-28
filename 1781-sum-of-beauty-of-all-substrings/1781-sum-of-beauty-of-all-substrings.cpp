812e20c68cb90470d9dd558aa197dcfd46489651
adarsh1722 <adarsh0004singh@gmail.com>
2022-03-28 15:45:52 +0530

    Time: 307 ms (67.87%), Space: 6.4 MB (89.49%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,27 +1,5 @@
 class Solution {
 public:
-    int getmin(int mp[]){
-         int minn = INT_MAX;
-        for(int i = 0 ; i < 26 ; i++)
-        {
-            if(mp[i] != 0){
-                minn = min(minn , mp[i]);
-            }
-        }
-        return minn;
-        
-    }
-    int getmax(int mp[]){
-        int maxx = 0;
-        for(int i = 0 ; i < 26 ; i++)
-        {
-            if(mp[i] != 0){
-                maxx = max(maxx , mp[i]);
-            }
-        }
-        return maxx;
-        
-    }
     int beautySum(string s) {
         
         int n = s.size();
@@ -30,13 +8,24 @@ public:
         for(int i = 0 ; i < n ; i++)
         {
             int mp[26] ={0};
-            
             for(int j = i; j < n ; j++)
             {
-                mp[s[j] - 'a'] += 1 ;
+                mp[s[j]-'a'] += 1 ;
+                               
+                int maxx = INT_MIN , minn = INT_MAX;
                 
-               sum += getmax(mp) - getmin(mp);
-               
+                 for(int i = 0 ; i < 26 ; i++)
+                 {
+                     if(mp[i] > 0){
+                         
+                         maxx = max(maxx , mp[i]);
+                         minn = min(minn , mp[i]);
+                         
+                     }
+                 }
+                // cout << maxx << " "<< minn << endl;
+                sum += (maxx - minn);
+                
                
             }
         }
