77fa79040403d9fa51cf8cb7391f47c50cc3a815
adarsh1722 <adarsh0004singh@gmail.com>
2022-04-19 11:37:33 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -5,48 +5,46 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
-    bool cycle(int parent , vector<int>adj[] , vector<int>&vis , vector<int>&dfsVis){
-        
-        vis[parent] = 1;
-        dfsVis[parent] = 1;
-        
-        for(auto &nbr : adj[parent]){
-            if(!vis[nbr]){
-                
-                if(cycle(nbr , adj , vis , dfsVis)){
-                    return true;
-                }
-            }
-            else if(dfsVis[nbr]){
-                return true;
-            }
-            
-            
-        }
-        dfsVis[parent] = 0;
-        return false;
-        
-        
-    }
-    
     // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<int> adj[]) {
+        // code here
         
-        vector<int>vis(V+1 , 0) , dfsVis(V+1 , 0);
         
-        for(int i = 0 ; i < V ; i++){
-            
-            if(!vis[i]){
-                
-                if(cycle(i , adj , vis , dfsVis)){
-                    return true;
-                }
-            }
-            
-            
-        }
-        return false;
-        
+        vector<int>inDegree(V);
+	    
+	    for(int i = 0 ; i < V ; i++){
+	        
+	        for(auto &nbr : adj[i]){
+	            inDegree[nbr]++;
+	        }
+	        
+	    }
+	    
+	    queue<int>q;
+	    for(int i= 0 ; i < V ; i++){
+	        if(inDegree[i] == 0){
+	            q.push(i);
+	        }
+	    }
+	    
+	    vector<int>topo;
+	    
+	    while(!q.empty()){
+	        
+	        int node = q.front();
+	        q.pop();
+	        topo.push_back(node);
+	        for(auto it : adj[node]){
+	            inDegree[it]--;
+	            if(inDegree[it] == 0){
+	                q.push(it);
+	            }
+	         }
+	        
+	    }
+	    
+	    return !(topo.size() == V);
+
     }
 };
 
