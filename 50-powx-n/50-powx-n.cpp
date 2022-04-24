a753be157bd8f5f4e90e30d727f067bb574f0c09
adarsh1722 <adarsh0004singh@gmail.com>
2022-04-25 00:25:21 +0530

    Time: 4 ms (26.30%), Space: 6.2 MB (13.88%) - LeetHub

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -2,9 +2,25 @@ class Solution {
 public:
     double myPow(double x, int n) {
         
-        double ans = 0;
-        ans = (double)pow(x , n);
-        return ans;
+        double ans=1.0;
+         int sign=n;
+         if(n==0 || x==1)return 1;      // 1^x=1 && x^0=1
+         else{
+		 		 if(n<0)
+                 n=abs(n);
+             //odd
+             if(n&1)
+                 ans=x*pow(x,n-1);
+             //even
+             else
+                 ans=ans*pow(x*x,n/2);
+            
+         }
+         if(sign<0)return 1.0/ans; 
+         return ans;
+        
+        
+        
           
         
     }
