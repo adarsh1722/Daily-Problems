8f923bfc428ca00769562427890e91399fd8b260
adarsh1722 <adarsh0004singh@gmail.com>
2022-04-25 14:45:58 +0530

    Time: 12 ms (84.11%), Space: 7.7 MB (40.07%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     void deleteNode(ListNode* node) {
         
-        /*update the current node value with its adjacent nodes values*/
+        /*update the current node value 
+        with its adjacent nodes values*/
         node->val = node->next->val;
-        /*update the address of the current node with the address stored in adjacent node*/
+        /*update the address of the current node
+        with the address stored in adjacent node*/
         node->next = node->next->next;
         
     }
