9d449834734d7a38c099c335004c9a2bcd119298
adarsh1722 <adarsh0004singh@gmail.com>
2022-04-27 12:12:00 +0530

    Time: 192 ms (75.19%), Space: 60.7 MB (93.64%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        
+        // Approach : Using two arrays as indegree and outdegree
+        
+        
+        vector<int>indegree(n+1 , 0);
+        
+        // treating the element of array as edge  a -> b
+        for(auto &x : trust)
+        {
+            indegree[x[0]]--;
+            indegree[x[1]]++;
+        }
+        
+        for(int i  = 1 ; i<= n ; i++)
+        {
+            if(indegree[i] == n-1)
+                return i;
+        }
+        return -1;
+        
+        
+        
+                
+    }
+};
