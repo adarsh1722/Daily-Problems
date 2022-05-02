6dd54f6ccd3e794fab72dda3c835e2b6e80fd1d1
adarsh1722 <adarsh0004singh@gmail.com>
2022-05-02 20:48:16 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (16.12%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    int countways(int n , vector<int>&dp){
-        if(n <= 1){
-            return 1;
-        }
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        return dp[n] = countways(n-1 ,dp) + countways(n-2 , dp);
-        
-    }
+   
     int climbStairs(int n) {
         
         vector<int>dp(n+1 , -1);
-        return countways(n , dp);
+        dp[0] = dp[1] = 1;
+        for(int i = 2 ; i <= n ; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
         
     }
 };
