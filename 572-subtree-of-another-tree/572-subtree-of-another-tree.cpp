9e7b2f642f712520fb0581e34550500bc485b6bd
adarsh1722 <adarsh0004singh@gmail.com>
2022-05-20 13:17:45 +0530

    Time: 26 ms (77.40%), Space: 28.8 MB (56.91%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.cpp
@@ -8,20 +8,17 @@ public:
         }
         return p->val == q->val && check(p->left , q->left ) && check(p->right , q->right);
     }
-    bool dfs(TreeNode* r1 , TreeNode* r2){
-        if(!r1) return false; ;
-        if(r1->val == r2->val){
-            if (check(r1  , r2)){
-               return true;
-            }
-        }
-        bool left = dfs(r1->left , r2);
-        bool right = dfs(r1->right , r2);
-        return left || right;
-    }
-    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+    
+    bool isSubtree(TreeNode* r1 , TreeNode* r2) {
         
-        return dfs(root , subRoot);
-        // return flag;
+        if(!r1) return false;
+        if(check(r1 , r2)){
+            return true;
+        }
+        
+        return isSubtree(r1->left , r2) || isSubtree(r1->right , r2);
+        
+        
+        
     }
 };
