8298d2a575cb80d526bf7135d10d2ea1bff1138f
adarsh1722 <adarsh0004singh@gmail.com>
2022-05-20 20:51:04 +0530

    Time: 26 ms (14.70%), Space: 20.2 MB (29.72%) - LeetHub

--- 988-smallest-string-starting-from-leaf/988-smallest-string-starting-from-leaf.cpp
+++ 988-smallest-string-starting-from-leaf/988-smallest-string-starting-from-leaf.cpp
@@ -6,20 +6,21 @@ public:
         if(!root){
             return;
         }
+        s += string(1 , 'a' + root->val);
         if(root->left == NULL && root->right == NULL){
             reverse(s.begin(),s.end());
             v.push_back(s);
             return ;
         }
-        if(root->left)  fun(root->left , v , s + string(1 , 'a' + root->left->val));
-        if(root->right) fun(root->right, v , s + string(1 , 'a' + root->right->val));
+        fun(root->left , v , s );
+        fun(root->right, v , s );
     }
     
     string smallestFromLeaf(TreeNode* root) {
         vector<string>v;
         if(root==NULL) return "";
         string s="";
-        s+=string(1 , 'a' + root->val);
+        // s+=string(1 , 'a' + root->val);
         fun(root,v,s);
         sort(v.begin(),v.end());
         return v[0];
