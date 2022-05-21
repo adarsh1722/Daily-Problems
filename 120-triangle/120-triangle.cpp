ac93636457258847742d43901dab7ac25b65361c
adarsh1722 <adarsh0004singh@gmail.com>
2022-05-21 13:08:36 +0530

    Time: 10 ms (39.50%), Space: 8.8 MB (45.10%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,32 +1,23 @@
 class Solution {
 public:
-    int f(int i  , int j , vector<vector<int>>&arr , vector<vector<int>> &dp){
-        int n = arr.size();
-        if(i == n - 1) return arr[n - 1][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        int d = arr[i][j]  + f(i+1 , j , arr , dp);
-        int dig = arr[i][j] + f(i+1 , j+1 ,arr , dp);
-        
-        return dp[i][j] = min(d , dig);
-        
-    }
-    int minimumTotal(vector<vector<int>>& arr) {
-        
-        int n = arr.size();
-        vector<vector<int>>dp(n , vector<int>(n ,-1));
-         
+    int minimumTotal(vector<vector<int>>& a) {
+        int n = a.size();
+        vector<int>front(n,0);
         for(int j = 0 ; j < n ; j++){
-            dp[n-1][j] = arr[n-1][j];
-        } 
-        for(int i = n-2 ;i >= 0 ; i--){
-            for(int j = 0 ; j <= i ; j++){
-                int d = arr[i][j] + dp[i+1][j];
-                int dig = arr[i][j] + dp[i+1][j+1];
-                dp[i][j] = min(d , dig);
-            }
+            front[j] = a[n-1][j];
         }
-        return dp[0][0];
-         
         
+        for(int i= n-2 ; i>= 0; i--){
+            
+            vector<int>curr(n ,  0);
+            for(int j = i ; j>=0 ;j--){
+                int d = a[i][j] + front[j];
+                int dig = a[i][j] + front[j+1];
+                curr[j] = min(d , dig);
+            }
+            front = curr;
+            
+        }
+        return front[0];
     }
 };
