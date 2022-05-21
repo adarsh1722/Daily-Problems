d1d1f08b26062e860abac8f7ebc6c8fc0d95d68f
adarsh1722 <adarsh0004singh@gmail.com>
2022-05-21 12:51:56 +0530

    Time: 6 ms (78.34%), Space: 9 MB (25.20%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int f(int i , int j , int n , vector<vector<int>>&triangle , vector<vector<int>>&dp){
-        if(i == n-1) return triangle[n-1][j];
+    int f(int i  , int j , vector<vector<int>>&arr , vector<vector<int>> &dp){
+        int n = arr.size();
+        if(i == n - 1) return arr[n - 1][j];
         if(dp[i][j] != -1) return dp[i][j];
-        int d = triangle[i][j] + f(i+1 , j , n , triangle , dp);
-        int dg = triangle[i][j] + f(i+1 , j+1 , n ,triangle , dp);
-        return dp[i][j] = min(d , dg);
+        int d = arr[i][j]  + f(i+1 , j , arr , dp);
+        int dig = arr[i][j] + f(i+1 , j+1 ,arr , dp);
+        
+        return dp[i][j] = min(d , dig);
         
     }
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int n = triangle.size();
-        vector<vector<int>>dp(n , vector<int>(n ,-1));
-        return f(0 , 0 , n , triangle , dp);
+    int minimumTotal(vector<vector<int>>& arr) {
         
+        int n = arr.size();
+        vector<vector<int>>dp(n , vector<int>(n ,-1));
+        return f(0 , 0 , arr , dp);
     }
 };
