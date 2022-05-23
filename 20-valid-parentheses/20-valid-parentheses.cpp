e7aab4382526225606331eba3827d0b68d97ae9c
adarsh1722 <adarsh0004singh@gmail.com>
2022-05-23 11:07:58 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (51.75%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     bool isValid(string s) {
         
-        
         stack<char>st;
         
-        for(auto  x : s){
-            
-            if(x == '(' || x == '{' || x == '['){
+        for(auto x : s){
+            if(x == '(' || x == '[' || x == '{'){
                 st.push(x);
             }
             else{
-                
-                /* example -     ][] (unbalanced because one of one closing bracket at starting )
-                                     stack will be empty */
-                if(st.empty() == true){
+                if(st.empty()){
                     return false;
                 }
                 
@@ -23,16 +18,9 @@ public:
                 if(x == ')' && st.top() != '(') return false;
                 
                 st.pop();
-                
             }
-                       
         }
         
-        
-        // Example - {}[({ (something like this where a opening bracket dont have  closing bracket)
-        if(st.empty()) return true; // when stack is empty it means that we have balanced paranthesis 
-        else return false;
-      
-        
+        return st.empty();
     }
 };
