a9b478b008399ecc95e3ee135623dd8a49db36af
adarsh1722 <adarsh0004singh@gmail.com>
2022-05-27 00:19:19 +0530

    Time: 402 ms (24.09%), Space: 12 MB (59.17%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,28 +1,42 @@
 class Solution {
 public:
-    int f(int idx , int k , vector<int>&nums , vector<vector<int>>&dp){
-        if(k == 0){
-            return true;
+    bool canPartition(int n  , vector<int>&arr){
+        int totSum = 0 ;
+        for(auto x : arr)
+            totSum += x;
+        
+        if(totSum %2 == 1) return false;
+         else{
+        int k = totSum/2;
+        vector<vector<bool>> dp(n,vector<bool>(k+1,false));
+    
+        for(int i=0; i<n; i++){
+            dp[i][0] = true;
         }
-        if(idx == 0) return nums[0] == k;
-        if(dp[idx][k] != -1) return dp[idx][k];
         
-        bool nottaken = f(idx-1 , k , nums , dp);
-        bool taken = false;
-        if(k >= nums[idx])
-            taken = f(idx-1 , k - nums[idx], nums , dp);
+        if(arr[0]<=k)
+            dp[0][arr[0]] = true;
         
-        return dp[idx][k] = taken || nottaken;
+        for(int ind = 1; ind<n; ind++){
+            for(int target= 1; target<=k; target++){
+                
+                bool notTaken = dp[ind-1][target];
+        
+                bool taken = false;
+                    if(arr[ind]<=target)
+                        taken = dp[ind-1][target-arr[ind]];
+            
+                dp[ind][target]= notTaken||taken;
+            }
+        }
+        
+        return dp[n-1][k];
+
+    } 
     }
     bool canPartition(vector<int>& nums) {
         
-        int sum = 0;
-        sum = accumulate(nums.begin() , nums.end(), 0);
-        if(sum&1) return false;
-        sum /= 2;
-        int n = nums.size();
-        vector<vector<int>>dp(n ,vector<int>(sum+1 , -1));
-        return f(n-1 , sum , nums , dp);
-        
+       int n = nums.size();
+       return canPartition(n , nums);
     }
 };
