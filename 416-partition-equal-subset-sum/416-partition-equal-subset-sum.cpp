6e5716b8115dc09492e03cce0a24cbec22b7f488
adarsh1722 <adarsh0004singh@gmail.com>
2022-05-27 00:01:06 +0530

    Time: 302 ms (40.54%), Space: 75.3 MB (23.92%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    bool f(int ind , vector<int>&nums , vector<vector<int>>&dp, int sum){
-        if(sum == 0) return true;
-        if(ind == 0) return nums[0] == sum;
-        if(dp[ind][sum] != -1) return dp[ind][sum];
-        bool nottake = f(ind-1 , nums , dp , sum);
-        bool take = false;
-        if(sum >= nums[ind])
-            take = f(ind-1  , nums , dp , sum-nums[ind]);
+    int f(int idx , int k , vector<int>&nums , vector<vector<int>>&dp){
+        if(k == 0){
+            return true;
+        }
+        if(idx == 0) return nums[0] == k;
+        if(dp[idx][k] != -1) return dp[idx][k];
         
-        return dp[ind][sum] =  take || nottake;
+        bool nottaken = f(idx-1 , k , nums , dp);
+        bool taken = false;
+        if(k >= nums[idx])
+            taken = f(idx-1 , k - nums[idx], nums , dp);
         
+        return dp[idx][k] = taken || nottaken;
     }
     bool canPartition(vector<int>& nums) {
-       
-        int sum  = 0;
-        for(auto x : nums){
-            sum += x;
-        }
-        if(sum&1) return false;
         
+        int sum = 0;
+        sum = accumulate(nums.begin() , nums.end(), 0);
+        if(sum&1) return false;
         sum /= 2;
         int n = nums.size();
-        vector<vector<int>>dp(n , vector<int>(sum+1 , -1));
-        return f(n-1 , nums , dp , sum);
+        vector<vector<int>>dp(n ,vector<int>(sum+1 , -1));
+        return f(n-1 , sum , nums , dp);
+        
     }
 };
