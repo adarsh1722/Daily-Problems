0896755b01113ff5e8798ea112208755785c11a3
adarsh1722 <adarsh0004singh@gmail.com>
2022-06-02 13:19:11 +0530

    Time: 16 ms (73.00%), Space: 10.5 MB (65.49%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int>sub;
+        for(auto x : nums){
+            if(sub.empty() || sub.back() < x){
+                sub.push_back(x);
+            }
+            else{
+                auto it = lower_bound(sub.begin() , sub.end() , x);
+                *it = x;
+            }
+        }
+        return sub.size();
+    }
+};
