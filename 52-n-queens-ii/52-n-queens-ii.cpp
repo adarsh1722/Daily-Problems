68f1c04b817d73c013f5a6fc8a5b4436b8fb2efe
adarsh1722 <adarsh0004singh@gmail.com>
2022-06-05 11:07:25 +0530

    Time: 77 ms (11.28%), Space: 44.6 MB (11.70%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -29,32 +29,34 @@ public:
         
         
     }
-    void solve(int col , vector<string>&board , set<vector<string>>&ans , int n) {
+    int solve(int col , vector<string>&board , int n) {
         if(col == n){
-            ans.insert(board);
-            return ;
+            return 1;
         }
+        int count = 0 ;
         for(int row = 0 ; row  < n ; row++){
             if(isSafe(row , col , board , n)){
                 board[row][col] = 'Q';
-                solve(col + 1 , board , ans , n);
+                count += solve(col + 1 , board , n);
                 board[row][col] = '.';
             }
         }
+        
+        return count;
         
         
     }
     int totalNQueens(int n) {
         
-        set<vector<string>>ans;
+      
         vector<string>board(n);
         string s(n , '.');
         for(int i = 0 ; i < n ; i++){
             board[i] = s;
         }
-        solve(0  , board , ans , n);
+        return solve(0  , board , n);
         
-        return ans.size();
+
         
         
     }
