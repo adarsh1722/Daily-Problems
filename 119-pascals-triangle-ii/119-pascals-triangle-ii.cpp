2a342651ab131705b890c51bf76139652ccab973
adarsh1722 <adarsh0004singh@gmail.com>
2022-06-25 20:45:13 +0530

    Time: 6 ms (12.93%), Space: 6.5 MB (50.04%) - LeetHub

---
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getRow(int n) {
+        
+        vector<vector<int>>dp(n+1);
+        for(int i = 0 ; i < n+1 ; i++){
+            dp[i].resize(i+1);
+            dp[i][0] = dp[i][i] = 1;
+            for(int j = 1 ; j < i ; j++){
+                dp[i][j]  = dp[i-1][j-1] + dp[i-1][j];
+            }
+        }
+        return dp[n];
+        
+    }
+};
