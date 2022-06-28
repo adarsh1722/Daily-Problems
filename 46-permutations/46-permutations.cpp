32816c7148c536f87390acde0a1f832395773cf5
adarsh1722 <adarsh0004singh@gmail.com>
2022-06-28 09:46:29 +0530

    Time: 14 ms (8.01%), Space: 8.3 MB (17.57%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,24 +1,30 @@
 class Solution {
 public:
-    void recurPermute(int index , vector<int>&nums , vector<vector<int>>& ans)
-    {
-        if(index == nums.size()){
-            ans.push_back(nums);
-            return;
+    void solve(vector<int>&nums , vector<int>ds , vector<vector<int>>&ans , vector<int>&hashMap){
+        
+        if(ds.size() == nums.size()){
+            ans.push_back(ds);
+            return ;
         }
-        for(int i = index ; i < nums.size() ; i++){
-            swap(nums[index] , nums[i]);
-            recurPermute(index + 1 , nums , ans);
-            swap(nums[index] , nums[i]);
+        
+        for(int i = 0; i < nums.size() ; i++){
+            
+            if(hashMap[i] == -1){
+                hashMap[i] = 1;
+                ds.push_back(nums[i]);
+                solve(nums , ds , ans , hashMap);
+                ds.pop_back();
+                hashMap[i] = -1;
+            }
         }
+        
         
     }
     vector<vector<int>> permute(vector<int>& nums) {
         
         vector<vector<int>>ans;
-        recurPermute(0 , nums , ans);
+        vector<int>ds , hashMap(nums.size() , -1);
+        solve(nums ,ds , ans , hashMap);
         return ans;
-        
     }
-    
 };
