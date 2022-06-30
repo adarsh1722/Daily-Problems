0abd6b5e998e8b87ffa3b3d608302a51165578c4
adarsh1722 <adarsh0004singh@gmail.com>
2022-06-30 12:03:22 +0530

    Time: 56 ms (14.69%), Space: 27.6 MB (68.45%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.cpp
@@ -1,22 +1,26 @@
 
 class Solution {
 public:
-    int maxPathDown(TreeNode*root , int &maxi){
-        
+    int maxSum(TreeNode*root ,int& maxi){
         if(!root){
             return 0;
         }
-        int left = max(0 , maxPathDown(root->left , maxi) );
-        int right = max(0 , maxPathDown(root->right , maxi));
+        
+        int left = max(0 , maxSum(root->left , maxi));
+        int right = max(0 , maxSum(root->right , maxi));
         
         maxi = max(maxi , left + right + root->val);
-        return max(left , right) + root->val;
+        
+        return max(left , right ) + root->val;
         
     }
     int maxPathSum(TreeNode* root) {
         
-        int maxi = INT_MIN;
-        maxPathDown(root , maxi);
+        if(!root){
+            return 0;
+        }
+        int maxi  = INT_MIN;
+        maxSum(root , maxi);
         return maxi;
         
     }
