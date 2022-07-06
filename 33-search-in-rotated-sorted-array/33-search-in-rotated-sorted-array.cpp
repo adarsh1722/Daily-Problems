6100a6c7fe82057b1ad7ae6fc1aaf9a3d85cab97
adarsh1722 <adarsh0004singh@gmail.com>
2022-07-06 14:15:56 +0530

    Time: 8 ms (37.03%), Space: 11.1 MB (74.88%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,38 +1,57 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
+    int solve(vector<int>&nums , int l , int h , int x){
         
-        int n = nums.size();
-        int low = 0 , high = n -1;
-        while(low <= high)
-        {
-            int mid = (low + high) / 2;
+        while(l <= h){
             
-            // if mid is the position  where target exists
-            if(nums[mid] == target){
+            int mid = l + (h - l)/2;
+            if(nums[mid] == x){
                 return mid;
             }
+            else if(nums[mid] < x) l = mid+1;
+            else h = mid - 1;
+                       
+        }
+        return -1;
+        
+        
+    }
+    int getminimum(vector<int>&nums){
+        int n = nums.size();
+        int start = 0 , end = n - 1;
+        while(start <= end){
             
-            if(nums[low] <= nums[mid]){
-                if(target >= nums[low] && target <= nums[mid]){
-                    high = mid -1;
-                }
-                else{
-                    low = mid + 1;
-                }
+            int mid = start + (end - start)/2;
+            int next = (mid+1)%n;
+            int prev = (mid + n - 1)%n;
+            
+            if(nums[mid] <= nums[next] && nums[mid] <= nums[prev]){
+                return mid;
+            }
+            else if(nums[mid] <= nums[end]){
+                end = mid - 1;
             }
             else{
-                if(target >= nums[mid] && target <= nums[high]){
-                    low = mid + 1;
-                }
-                else{
-                    high = mid - 1;
-                }
+                start = mid + 1;
             }
             
             
         }
         return -1;
+    }
+    int search(vector<int>& nums, int target) {
+        
+        int idx = -1 , n = nums.size();
+        idx = getminimum(nums);
+        
+        int left = solve(nums , 0 , idx-1 , target );
+        
+        int right = solve(nums , idx , n -1 ,target);
+        
+        if(left != -1) return left;
+        if(right != -1) return right;
+        return -1;
+        
         
     }
 };
