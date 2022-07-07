34942991ead9024f9790434b525dcac900ee8468
adarsh1722 <adarsh0004singh@gmail.com>
2022-07-07 18:29:00 +0530

    Time: 7 ms (78.88%), Space: 9 MB (36.11%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& a) {
-        int n = a.size();
-        vector<int>front(n,0);
-        for(int j = 0 ; j < n ; j++){
-            front[j] = a[n-1][j];
-        }
+    int solve(int i , int j , vector<vector<int>>&triangle  , int n , vector<vector<int>>&dp) {
+        if(i == n-1) return triangle[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
+        int down = triangle[i][j] + solve(i+1 , j , triangle , n , dp);
+        int diag = triangle[i][j] + solve(i+1 , j + 1 , triangle , n , dp);
         
-        for(int i= n-2 ; i>= 0; i--){
-            
-            vector<int>curr(n ,  0);
-            for(int j = i ; j>=0 ;j--){
-                int d = a[i][j] + front[j];
-                int dig = a[i][j] + front[j+1];
-                curr[j] = min(d , dig);
-            }
-            front = curr;
-            
-        }
-        return front[0];
+        return dp[i][j] =  min(down , diag);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        
+        int n = triangle.size();
+        vector<vector<int>>dp(n ,vector<int>(n , -1));
+        return solve(0 , 0 , triangle , n , dp);
+        
     }
 };
