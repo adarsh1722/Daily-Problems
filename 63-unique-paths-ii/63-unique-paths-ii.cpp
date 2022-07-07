5a2c7f79dce87531af7b17ab8397e9eec310cf17
adarsh1722 <adarsh0004singh@gmail.com>
2022-07-07 13:59:11 +0530

    Time: 6 ms (42.71%), Space: 7.9 MB (12.49%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -23,7 +23,28 @@ public:
         
         int m = g.size() , n = g[0].size();
         vector<vector<int>>dp(m ,vector<int>(n , -1));
-        return solve(m -1 ,  n-1 , g , dp);
+        
+        if(g[0][0] == 1 || g[m-1][n-1] == 1){
+            return  0;
+        }
+        
+        dp[0][0] = 1;
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(i == 0 &&  j == 0 ){
+                    continue;
+                }
+                
+                 int up = 0  , left = 0;
+        
+                if(i > 0 && g[i-1][j] != 1) up = dp[i-1][j];
+                if(j > 0 && g[i][j-1] != 1) left = dp[i][j-1] ;
+                dp[i][j] =  up + left;
+                                
+            }
+        }
+        
+        return dp[m-1][n-1];
         
         
         
