3a9ebafff14cb9f89cc31f75328b1cc788ceec7a
adarsh1722 <adarsh0004singh@gmail.com>
2022-07-14 15:25:30 +0530

    Added solution - LeetHub

--- Count Inversions - GFG/count-inversions.cpp
+++ Count Inversions - GFG/count-inversions.cpp
@@ -6,6 +6,9 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
   public:
+    // arr[]: Input Array
+    // N : Size of the Array arr[]
+    // Function to count inversions in the array.
     long long int merge(long long arr[] , long long temp[] , int left , int mid , int right)
     {
         long long int i , j , k , inv_count = 0;
@@ -36,23 +39,26 @@ class Solution{
         return inv_count;
         
     }
-    
-    long long int mergeSort(long long arr[] , long long temp[] , int left , int right)
-    {
-        long long int mid = 0 , inv_count = 0;
-        if(right > left)
-        {
-            mid = (right + left)/2;
-            inv_count += mergeSort(arr, temp , left , mid);
-            inv_count += mergeSort(arr , temp , mid + 1 , right);
-            inv_count += merge(arr , temp , left , mid+1 , right);
+
+    long long int mergeSort(long long arr[] , long long temp[] , int left , int right){
+        
+        long long int mid = 0  , ans = 0;
+        if(right > left){
+            
+                mid = (left + right) /2;
+                ans += mergeSort(arr ,temp , left , mid);
+                ans += mergeSort(arr ,temp , mid +1 , right);
+                ans += merge(arr ,temp , left , mid+1 , right);
         }
-        return inv_count ;
+       
+        return ans;
+        
     }
     long long int inversionCount(long long arr[], long long N)
     {
         long long int temp[N];
-        return mergeSort(arr , temp , 0 , N - 1);
+        return mergeSort(arr,  temp , 0 , N-1);
+        
     }
 
 };
