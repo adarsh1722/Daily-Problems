8e5f500876adea9491e973c2d4e14b19d559fcaa
adarsh1722 <adarsh0004singh@gmail.com>
2022-07-20 13:31:52 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (31.97%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -2,16 +2,21 @@ class Solution {
 public:
     vector<vector<int>> generate(int n) {
         
-        vector<vector<int>>dp(n);
+        vector<vector<int>>res(n);
         
         for(int i = 0 ; i < n ; i++){
-            dp[i].resize(i+1);
-            dp[i][0] = dp[i][i] = 1;
+            
+            res[i].resize(i+1);
+            res[i][0] = res[i][i] = 1;
+            
             for(int j = 1 ; j < i ; j++){
-                dp[i][j] = dp[i-1][j] + dp[i-1][j-1];
+                res[i][j] = res[i-1][j] + res[i-1][j-1];
             }
+                        
         }
-        return dp;
+        return res;
+        
+        
         
     }
 };
