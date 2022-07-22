9c835d517d6a254c86505dd6080cc5305966c624
adarsh1722 <adarsh0004singh@gmail.com>
2022-07-22 22:44:46 +0530

    Time: 7 ms (85.56%), Space: 13.2 MB (47.28%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        
         if(grid.empty()) return 0;
         
-        int n = grid.size()  , m  = grid[0].size() ,days= 0 , tot = 0 , cnt =0;
-        queue<pair<int , int>>rotten;
+        int n = grid.size() , m = grid[0].size() , tot = 0  , cnt = 0 , days = 0 ;
+        queue<pair<int  , int>>q;
         
-        for(int i = 0 ; i < n ; i++ ){
-            for(int j = 0 ; j < m ; j++ ){
-                // counting all the oranges whether it is fresh or not
+        for(int i  = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
                 if(grid[i][j] != 0) tot++;
-                if(grid[i][j] == 2) rotten.push({i , j});
+                if(grid[i][j] == 2) q.push({i , j});
             }
         }
         
-        int dx[4] = {-1 , 0 , 1, 0};
-        int dy[4] = {0 , 1, 0 , -1};
-        while(!rotten.empty()){
-            int k = rotten.size();
+        int dx[4]  = {-1 , 0 , 1 , 0};
+        int dy[4]  = {0 , 1 , 0 , -1};
+        
+        while(!q.empty()){
+            
+            int k = q.size();
             cnt += k;
+           
             while(k--){
+                int x = q.front().first , y = q.front().second;
+                q.pop();
                 
-                int x = rotten.front().first , y = rotten.front().second;
-                rotten.pop();
-                
-                for(int i = 0  ; i < 4 ; i++){
+                for(int i = 0 ; i < 4 ; i++){
                     int nx = x + dx[i] , ny = y + dy[i];
-                    if(nx < 0 || ny < 0 || ny >= m || nx >= n || grid[nx][ny] != 1)continue;
-                    // if it is fresh then only you will come to this part
+                    if(nx < 0 || ny < 0 || ny >= m || nx >= n || grid[nx][ny] != 1)
+                        continue;
                     grid[nx][ny] = 2;
-                    rotten.push({nx, ny});
-                }
-              
+                    q.push({nx , ny});
+                    
+                }            
+                
             }
-            // days will only be incremented only if they are any element in queue
-            if(!rotten.empty()) days++;
+            if(!q.empty())  days += 1;
+           
+            
         }
-        
-        return tot == cnt ?days : -1;
+        return tot == cnt ? days: -1;
         
     }
 };
