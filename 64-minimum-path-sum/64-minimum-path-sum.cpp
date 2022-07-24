0dd05686b4ad47ba89715f0f086e2703cb4a5093
adarsh1722 <adarsh0004singh@gmail.com>
2022-07-25 00:47:53 +0530

    Time: 24 ms (12.51%), Space: 10 MB (52.10%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    int f(int i , int j , vector<vector<int>>& g , vector<vector<int>>&dp){
+    int f(int i , int j , vector<vector<int>>&g , vector<vector<int>>&dp){
         if(i == 0 && j == 0){
             return g[0][0];
-        
         }
         if(i < 0 || j < 0) return 1e9;
+        
         if(dp[i][j] != -1) return dp[i][j];
-        int up = g[i][j] +  f(i-1, j , g , dp);
-        int left = g[i][j] + f(i , j -1 , g , dp);
-        return dp[i][j] = min(up , left);
+        
+        int up = g[i][j] + f(i-1 , j , g , dp);
+        int left = g[i][j] + f(i , j-1 , g , dp);
+        
+        return dp[i][j] = min(up , left);        
         
     }
     int minPathSum(vector<vector<int>>& g) {
+        int n = g.size() , m = g[0].size();
         
-        int m = g.size() , n = g[0].size();
-        vector<vector<int>>dp(m , vector(n , -1));
-        return f(m-1 , n-1, g , dp);
+        vector<vector<int>>dp(n , vector<int>(m , -1));
+        return f(n - 1, m-1 , g , dp);
+        
+        
         
     }
 };
