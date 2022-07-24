1259149d81312ffda7df5d7234df9e522b9a278e
adarsh1722 <adarsh0004singh@gmail.com>
2022-07-25 01:18:51 +0530

    Time: 18 ms (8.96%), Space: 9 MB (22.53%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -19,9 +19,21 @@ public:
         
         
         int n = arr.size();
-        vector<vector<int>>dp(n , vector<int>(n , -1));
-        return solve(0 , 0 , arr , n , dp);
+        vector<vector<int>>dp(n , vector<int>(n , 0));
         
+        for(int j = 0 ; j < n ; j++){
+            dp[n-1][j] = arr[n - 1][j];
+            
+        }
+        
+        for(int i = n-2 ; i >= 0 ; i--){
+            
+            for(int j = 0 ; j<= i ;j++){
+                dp[i][j] = min(arr[i][j] + dp[i+1][j] , arr[i][j] + dp[i+1][j+1]);
+            }
+            
+        }
+        return dp[0][0];
         
         
     }
