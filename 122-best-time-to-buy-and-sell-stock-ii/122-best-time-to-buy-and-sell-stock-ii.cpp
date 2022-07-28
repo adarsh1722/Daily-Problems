e49bca678b58dabf30a365f81f0bb750a4f6a189
adarsh1722 <adarsh0004singh@gmail.com>
2022-07-29 00:04:38 +0530

    Time: 32 ms (5.11%), Space: 17.7 MB (5.12%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
-    int f(int ind , int buy , vector<int>&prices , int n , vector<vector<int>>&dp){
-        if(ind == n){
+    int solve(int ind , int buy  , vector<int>&arr , vector<vector<int>>&dp){
+        if(ind == arr.size()){
             return 0;
         }
+        
         if(dp[ind][buy] != -1) return dp[ind][buy];
         long profit = 0;
-        if(buy){
-            profit = max(-prices[ind] + f(ind+1, 0 , prices , n , dp ) , 
-                        0 + f(ind+1 , 1 , prices , n , dp));
+        
+        // i can buy a stock
+        if(buy == 0){
+            
+            profit = max(0 + solve(ind+1 , 0 , arr , dp) , -arr[ind] + solve(ind+1 , 1 , arr ,dp));
         }
-        else{
-            profit = max(prices[ind] + f(ind+1 , 1 , prices , n , dp),
-                        0 + f(ind+1 , 0 , prices , n , dp));
+        else{ // i have to sell it
+            profit = max(0 + solve(ind+1 , 1 , arr , dp) , arr[ind] + solve(ind+1 , 0 , arr , dp));
         }
+        
         return dp[ind][buy] = profit;
     }
     int maxProfit(vector<int>& prices) {
-       
         int n = prices.size();
-        vector<vector<int>>dp(n+1 , vector<int>(2 , -1));
-        dp[n][0] = dp[n][1] = 0;
-        
-        for(int ind = n-1 ; ind >= 0 ; ind--){
-            for(int buy = 0 ; buy <= 1 ; buy++){
-                 long profit = 0;
-                if(buy){
-                    profit = max(-prices[ind] + dp[ind+1][ 0 ], 
-                                0 + dp[ind+1][1]);
-                }
-                else{
-                    profit = max(prices[ind] + dp[ind+1][1] ,
-                                0 + dp[ind+1][0]);
-                }
-                dp[ind][buy] = profit;
-            }
-        }
-        return dp[0][1];
-        
-        
+        vector<vector<int>>dp(n , vector<int>(2  , -1));
+        return solve(0 , 0 , prices , dp);
     }
 };
