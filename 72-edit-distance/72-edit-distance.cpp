112b45644c786992b37a94f053f9974e8383d564
adarsh1722 <adarsh0004singh@gmail.com>
2022-07-28 12:46:10 +0530

    Time: 11 ms (84.72%), Space: 9.3 MB (18.22%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
-    int f(int i , int j , string s1 , string s2 ,vector<vector<int>>&dp ){
+    int f(int i , int j ,string &s1 , string &s2 , vector<vector<int>>&dp){
+        
         if(i == 0) return j;
         if(j == 0) return i;
+        
         if(dp[i][j] != -1) return dp[i][j];
-        if(s1[i-1] == s2[j-1]) {
-            return dp[i][j] = f(i-1 , j-1 , s1 , s2, dp);
-        }
+        
+        if(s1[i-1] == s2[j-1]) return dp[i][j] = 0 + f(i-1 , j-1 ,s1 ,s2 , dp);
         else{
-            return dp[i][j] = 1 + min(f(i-1 , j , s1 ,s2 , dp) , min(f(i , j-1  ,s1 , s2 , dp) , f(i - 1 , j-1 , s1 ,s2 , dp) ));
+            return dp[i][j] = 1 + min({f(i-1 , j , s1 ,s2 , dp) , f(i , j-1 , s1 , s2, dp) , f(i-1 , j-1 ,s1  ,s2 , dp)});
+            
         }
+        
     }
-    int minDistance(string s1, string s2) {
-      
-        int n = s1.size();
-       int m = s2.size();
-       vector<vector<int>>dp(n+1 , vector<int>(m+1 , 0));
-       
-        for(int i = 0 ; i <=  n ; i++){
-            dp[i][0] = i;
-        }
-        for(int j = 0 ; j <= m  ;j++){
-            dp[0][j] = j;
-        }
+    int minDistance(string word1, string word2) {
         
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= m ; j++){
-                 if(s1[i-1] == s2[j-1]) {
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else{
-                    dp[i][j] = 1 + min(dp[i-1][j] , min(dp[i][j-1] , dp[i - 1][j-1]));
-                }
-            }
-        }
-        return dp[n][m];
-        
+        int n = word1.size() , m = word2.size();
+        vector<vector<int>>dp(n+1 , vector<int>(m+1 , -1));
+        return f(n, m , word1 , word2 , dp);
         
     }
 };
