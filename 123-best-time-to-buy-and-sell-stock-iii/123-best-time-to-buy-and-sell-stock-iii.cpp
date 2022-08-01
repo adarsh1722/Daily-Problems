005cc067627af68a7941bafecbb82b1944555648
adarsh1722 <adarsh0004singh@gmail.com>
2022-08-01 13:49:33 +0530

    Time: 1309 ms (13.37%), Space: 218.1 MB (9.20%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    int f(int ind , int buy , int cap , vector<int>&prices , int n ,  vector<vector<vector<int>>>&dp){
-        if(ind == n || cap == 0) return 0;
-        if(dp[ind][buy][cap] != -1) return dp[ind][buy][cap] ;
-        if(buy == 1){
-            return dp[ind][buy][cap] = max(-prices[ind] + f(ind+1 , 0 , cap , prices , n , dp),
-                      0 + f(ind+1 , 1 , cap, prices , n , dp));
+    int getAns(vector<int>& Arr, int n, int ind, int buy, int cap, vector<vector<vector<int>>>& dp){
+
+        if(ind==n || cap==0) return 0; //base case
+
+        if(dp[ind][buy][cap]!=-1)
+            return dp[ind][buy][cap];
+
+        int profit;
+
+        if(buy==0){// We can buy the stock
+            profit = max(0+getAns(Arr,n,ind+1,0,cap,dp), 
+                        -Arr[ind] + getAns(Arr,n,ind+1,1,cap,dp));
         }
-        else{
-             return dp[ind][buy][cap] = max(prices[ind] + f(ind+1 , 1 , cap-1, prices , n , dp),
-                      0 + f(ind+1 , 0 , cap, prices , n , dp));
+
+        if(buy==1){// We can sell the stock
+            profit = max(0+getAns(Arr,n,ind+1,1,cap,dp),
+                        Arr[ind] + getAns(Arr,n,ind+1,0,cap-1,dp));
         }
+
+        return dp[ind][buy][cap] = profit;
     }
     int maxProfit(vector<int>& prices) {
-        
         int n = prices.size();
-        vector<vector<vector<int>>>dp(n + 1 , vector<vector<int>>(2 , vector<int>(3 , 0)));
-        
-        for(int ind = n-1 ; ind >= 0 ; ind--){
-            for(int buy = 0 ; buy <= 1 ; buy++){
-                for(int cap = 1 ; cap <= 2 ; cap++){
-                    
-                    if(buy == 1){
-                        dp[ind][buy][cap] = max(-prices[ind] + dp[ind+1][0][cap],
-                                  0 +dp[ind+1][1][cap]);
-                    }
-                    else{
-                        dp[ind][buy][cap] = max(prices[ind] + dp[ind+1][1][cap-1],
-                                  0 + dp[ind+1][0][cap]);
-                    }
-                    
-                }
-            }
-        }
-        
-        return dp[0][1][2];
+        vector<vector<vector<int>>>dp(n , vector<vector<int>>(2 , vector<int>(3 , -1)));
+        return getAns(prices , n , 0 , 0 , 2 , dp);
     }
 };
