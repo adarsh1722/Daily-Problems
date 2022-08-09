7fbfa3ae7156da8b32d4c34629192a7bbc7e6fc4
adarsh1722 <adarsh0004singh@gmail.com>
2022-08-09 22:12:08 +0530

    Time: 20 ms (14.45%), Space: 15.2 MB (7.87%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -10,43 +10,31 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+       
+        vector<int>arr;
+        ListNode* t = list1;
         
-        ListNode* t1 = l1 , *t2 = l2;
-        ListNode dummy;
-        ListNode* tail = &dummy , *head = &dummy;
-        
-        while(t1 && t2)
-        { 
-            if(t1->val <= t2->val)
-            {
-                ListNode* temp = t1->next;
-                tail->next = t1;
-                t1->next = NULL;
-                tail = tail->next;
-                t1 = temp;
-                
-            }
-            else
-            {
-                ListNode* temp = t2->next;
-                tail->next = t2;
-                t2->next = NULL;
-                tail = tail->next;
-                t2 = temp;
-                
-            }
+        while(t){
+            arr.push_back(t->val);
+            t = t->next;
             
         }
+        t = list2;
+        while(t){
+            arr.push_back(t->val);
+            t = t->next;
+            
+        }
+        sort(arr.begin()  , arr.end());
+        ListNode* dummy =  new ListNode();
+        ListNode* tail = dummy;
         
-        if(t1)
-            tail->next = t1;
-        if(t2)
-            tail->next = t2;
-        
-        return head->next;
-        
-        
+        for(auto x : arr){
+            tail->next = new ListNode(x);
+            tail = tail->next;
+        }
+        return dummy->next;
         
         
         
