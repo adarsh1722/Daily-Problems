eac37067dd032aa3deed3758e2d5da4232520e65
adarsh1722 <adarsh0004singh@gmail.com>
2022-08-14 00:18:51 +0530

    Time: 21 ms (14.13%), Space: 13 MB (78.80%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,46 +1,59 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        if(grid.empty()) return 0;
         
-        int n = grid.size() , m = grid[0].size() , tot = 0  , cnt = 0 , days = 0 ;
-        queue<pair<int  , int>>q;
+        if(grid.size() == 0){
+            return 0;
+        }
         
-        for(int i  = 0 ; i < n ; i++){
+        int n = grid.size() , m = grid[0].size() , tot = 0 ,days = 0 ,cnt = 0;
+        queue<pair<int , int>>q;
+        for(int i = 0 ; i < n ; i++){
             for(int j = 0 ; j < m ; j++){
-                if(grid[i][j] != 0) tot++;
+                if(grid[i][j] != 0 ) tot++;
                 if(grid[i][j] == 2) q.push({i , j});
             }
         }
         
-        int dx[4]  = {-1 , 0 , 1 , 0};
-        int dy[4]  = {0 , 1 , 0 , -1};
+        int dx[4] = {-1 , 0 , 1, 0};
+        int dy[4] = {0 , 1, 0 , -1};
         
         while(!q.empty()){
             
-            int k = q.size();
-            cnt += k;
-            // days += 1;
-            while(k--){
-                int x = q.front().first , y = q.front().second;
+            int sz = q.size();
+            cnt += sz;
+            while(sz--){
+                
+                auto node = q.front();
                 q.pop();
+                int x = node.first;
+                int y = node.second;
                 
                 for(int i = 0 ; i < 4 ; i++){
-                    int nx = x + dx[i] , ny = y + dy[i];
-                    if(nx < 0 || ny < 0 || ny >= m || nx >= n || grid[nx][ny] != 1)
-                        continue;
+                    
+                    int nx = x + dx[i];
+                    int ny = y + dy[i];
+                    
+                    if(nx < 0 || ny < 0 || nx >= n || ny >= m || grid[nx][ny] != 1) continue;
+                    
+                    
                     grid[nx][ny] = 2;
                     q.push({nx , ny});
                     
-                }            
+                    
+                }
+                
+                
                 
             }
+            if(!q.empty())days++;
             
-            if(!q.empty()) days++;
-           
             
         }
-        return tot == cnt ? days: -1;
+        
+        return tot == cnt ? days : -1;
+
+        
         
     }
 };
