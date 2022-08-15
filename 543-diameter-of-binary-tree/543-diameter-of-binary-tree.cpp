a84e293b760acd26034474f7f184bd15d8c0482f
adarsh1722 <adarsh0004singh@gmail.com>
2022-08-15 21:31:56 +0530

    Time: 37 ms (6.50%), Space: 20.3 MB (26.58%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.cpp
@@ -1,20 +1,42 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int f(TreeNode* root , int &maxx){
-        if(!root) return 0;
-        int lH = f(root->left , maxx);
-        int rH = f(root->right , maxx);
-        maxx = max(maxx , lH + rH);
-        return 1 + max(lH , rH);
+    int ans = 0;
+    int getMaxDiameter(TreeNode* r){
+        if(!r) return 0;
+        
+        int lh = getMaxDiameter(r->left);
+        int rh = getMaxDiameter(r->right);
+        // ans = max(ans , lh + rh);
+        return 1 + max(lh , rh);
+    }
+    void dfs(TreeNode* r){
+        if(!r) return ;
+        
+        int lh = getMaxDiameter(r->left);
+        int rh = getMaxDiameter(r->right);
+        ans = max(ans , lh + rh);
+        dfs(r->left);
+        dfs(r->right);
+        
     }
     int diameterOfBinaryTree(TreeNode* root) {
+        ans = 0;
         
         if(!root) return 0;
-        int maxx = -1;
-        f(root , maxx);
-        return maxx;
-            
+        
+        dfs(root);
+        return ans;
         
         
     }
