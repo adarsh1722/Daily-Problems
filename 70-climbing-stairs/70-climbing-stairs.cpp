844f01a696eb602bf9341f372de5526ec152bb8f
adarsh1722 <adarsh0004singh@gmail.com>
2022-08-21 10:54:14 +0530

    Time: 5 ms (12.51%), Space: 6.1 MB (31.68%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -11,9 +11,14 @@ public:
     int climbStairs(int n) {
         
         if(n <= 1) return 1;
-        vector<int>dp(n+1 , -1);
-        return get(n , dp);
+        vector<int>dp(n+1 , 0);
+      
+        dp[0] = dp[1] = 1;
         
+        for(int  i = 2 ; i<= n ; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];        
         
     }
 };
