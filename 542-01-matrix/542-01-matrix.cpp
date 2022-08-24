7f1040c57a65e25d627c4229823673591bd3de9e
adarsh1722 <adarsh0004singh@gmail.com>
2022-08-24 21:17:46 +0530

    Time: 112 ms (62.61%), Space: 30.4 MB (27.48%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,42 +1,46 @@
 class Solution {
 public:
+    int dx[4]={-1,0,+1,0};
+    int dy[4]={0,+1,0,-1};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-      
-        int m = mat.size(), n = mat[0].size();
         
-        vector<vector<int>>res(m , vector<int>(n , INT_MAX));
-        
-        queue<pair<int ,int>>q;
-        
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
+        int n = mat.size() , m = mat[0].size();
+        vector<vector<int>>res( n , vector<int>(m , 1e9));
+        // push all the zeroes
+        queue<pair<int , int>>q;
+        for(int i = 0 ; i < n ; i++){
+            for(int  j = 0 ; j < m ; j++){
                 if(mat[i][j] == 0){
-                    res[i][j] = 0;
                     q.push({i , j});
+                    res[i][j] = 0;
                 }
             }
         }
         
-        vector<pair<int ,int>>dirs{{-1 ,0}, {1 , 0} ,{0 , 1} , {0 , -1}};
+        
+        
         
         while(!q.empty()){
-            auto cur = q.front(); q.pop();
-            int curx = cur.first;
-            int cury = cur.second;
             
-            for(auto dir : dirs){
-                int x = curx + dir.first;
-                int y = cury + dir.second;
+            auto [x , y] = q.front();
+            q.pop();
+            
+            for(int i = 0 ; i < 4 ; i++){
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 
-                if(x >= 0 && x < m && y >= 0 && y < n){
+                if(nx >= 0 && nx < n && ny >= 0 &&  ny < m && mat[nx][ny]== 1){
+                    res[nx][ny] = res[x][y] + 1;
+                    q.push({nx , ny});
+                    mat[nx][ny]= 0;
                     
-                    if(res[x][y] > res[curx][cury]+1){
-                        res[x][y] = res[curx][cury]+1;
-                        q.push({x , y});
-                    }
                 }
             }
+            
+            
         }
         return res;
+        
+        
     }
 };
