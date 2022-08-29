6f6f79d943a6fd599fca0d3575e2f87d0b79f1d4
adarsh1722 <adarsh0004singh@gmail.com>
2022-08-29 21:42:34 +0530

    Time: 8 ms (70.30%), Space: 9.1 MB (10.13%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
-    int solve(int i , int j , vector<vector<int>>&arr  , int n , vector<vector<int>>&dp) {
+    int solve(int i , int j  ,vector<vector<int>>& triangle ,vector<vector<int>>&dp){
         
-        if(i == n-1){
-            return arr[n-1][j];
-        }
-        
+        int n = triangle.size();
+        if(i == n-1) return triangle[n-1][j];
         if(dp[i][j] != -1) return dp[i][j];
         
-        int down = arr[i][j] + solve(i+1 , j , arr , n , dp);
-        int diag = arr[i][j] + solve(i+1 , j + 1 , arr , n ,dp);
+        int down = triangle[i][j] + solve(i+1 , j , triangle , dp);
+        int diag = triangle[i][j] + solve(i+1 , j+1 , triangle , dp);
         
-        return dp[i][j] = min(down  , diag);
+        return dp[i][j] = min(down ,diag);
         
         
     }
-    int minimumTotal(vector<vector<int>>& arr) {
+    int minimumTotal(vector<vector<int>>& triangle) {
         
         
-        int n = arr.size();
-        vector<vector<int>>dp(n , vector<int>(n , 0));
-        
-        for(int j = 0 ; j < n ; j++){
-            dp[n-1][j] = arr[n - 1][j];
-            
-        }
-        
-        for(int i = n-2 ; i >= 0 ; i--){
-            
-            for(int j = 0 ; j<= i ;j++){
-                dp[i][j] = min(arr[i][j] + dp[i+1][j] , arr[i][j] + dp[i+1][j+1]);
-            }
-            
-        }
-        return dp[0][0];
-        
+        int n = triangle.size();
+        vector<vector<int>>dp(n , vector<int>(n , -1));
+        return solve(0 , 0 , triangle , dp);
         
     }
 };
