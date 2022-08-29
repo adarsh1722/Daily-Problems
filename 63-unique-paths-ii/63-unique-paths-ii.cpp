4f197942f38f946a319d9d9d951c33f6af73a48f
adarsh1722 <adarsh0004singh@gmail.com>
2022-08-29 20:08:37 +0530

    Time: 4 ms (67.07%), Space: 7.9 MB (13.38%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,50 +1,20 @@
 class Solution {
 public:
-    int solve(int i , int j  , vector<vector<int>>& g , vector<vector<int>>&dp){
+    int fun(int i , int j, vector<vector<int>>&a , vector<vector<int>>&dp ){
+        if(i < 0 || j < 0) return 0;
+        if(a[i][j] == 1) return 0;
+        if(i == 0 && j == 0) return 1;
         
-        if(g[i][j] == 1){
-            return 0;
-        }
+        if(dp[i][j] != -1) return dp[i][j];
         
-        // Base Case;
-        if(i == 0 && j == 0){
-            return 1;
-        }
-        if(dp[i][j]  != -1) return dp[i][j];
-        
-        int up = 0  , left = 0;
-        
-        if(i > 0) up = solve(i-1 , j , g , dp);
-        if(j > 0) left = solve(i  , j -1 , g , dp);
-        return dp[i][j] =  up + left;
+        return dp[i][j] = fun(i-1 , j ,a , dp) + fun(i ,j-1 ,a , dp);
         
     }
-    int uniquePathsWithObstacles(vector<vector<int>>& g) {
+    int uniquePathsWithObstacles(vector<vector<int>>& a) {
         
-        int m = g.size() , n = g[0].size();
-        vector<vector<int>>dp(m ,vector<int>(n , -1));
-        
-        if(g[0][0] == 1 || g[m-1][n-1] == 1){
-            return  0;
-        }
-        
-        dp[0][0] = 1;
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
-                if(i == 0 &&  j == 0 ){
-                    continue;
-                }
-                
-                 int up = 0  , left = 0;
-        
-                if(i > 0 && g[i-1][j] != 1) up = dp[i-1][j];
-                if(j > 0 && g[i][j-1] != 1) left = dp[i][j-1] ;
-                dp[i][j] =  up + left;
-                                
-            }
-        }
-        
-        return dp[m-1][n-1];
+        int n = a.size() , m = a[0].size();
+        vector<vector<int>>dp(n , vector<int>(m , -1));
+        return fun(n-1 , m-1 , a , dp);
         
         
         
