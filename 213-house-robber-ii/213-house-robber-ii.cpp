f2517a7478d573da0d2b227db1a3925053634074
adarsh1722 <adarsh0004singh@gmail.com>
2022-08-29 18:38:59 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (6.87%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -3,7 +3,7 @@ public:
     
     int solve(int i  , vector<int>&nums , vector<int>&dp){
         
-        if(i < 0) return 0;
+        if(i < 0)return 0;
         if(i == 0)  return nums[0];
         if(dp[i] != -1) return dp[i];
         
@@ -19,8 +19,23 @@ public:
     int Rob(vector<int>&nums){
         
         int n = nums.size();
-        vector<int>dp(n , -1);
-        return solve(n-1 , nums , dp);
+        vector<int>dp(n , 0 );
+        dp[0] = nums[0];
+        
+        for(int i = 1 ; i< n ; i++){
+            
+            int notpick = dp[i-1];
+        
+            int pick = nums[i];
+            if(i-2 >= 0){
+                pick += dp[i-2];
+            }
+
+            dp[i] =  max(pick ,notpick);
+            
+        }
+        
+        return dp[n-1];
         
         
     }
