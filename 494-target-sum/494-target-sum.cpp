dba16819033e0fcc7140e661717685bb3e38b1ba
adarsh1722 <adarsh0004singh@gmail.com>
2022-08-31 11:30:25 +0530

    Time: 23 ms (63.40%), Space: 12.1 MB (44.69%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
-    int f(int i , int curr_sum , vector<int>&nums , int target){
-        // base case
+    int solve(int i , int sum , vector<int>&nums , vector<vector<int>>&dp){
+        
         if(i == 0){
-            int x = curr_sum + nums[0];
-            int y = curr_sum - nums[0];
-            
-            if(x == target && y  == target) return 2;
-            else if(x == target || y == target) return 1;
-            else return 0;
+            if(sum == 0 && nums[0] == 0) return 2;
+            if(sum == 0 || nums[0] == sum) return 1;
+            return 0;
         }
-        
-        // try out all possible ways
-        int plus = f(i-1 , curr_sum + nums[i] ,nums , target);
-        int minus = f(i-1 , curr_sum - nums[i] ,nums , target);
-        
-        // return ways
-        return plus + minus;
-        
+        if(dp[i][sum] != -1) return dp[i][sum];
+        int notpick = solve(i-1 , sum , nums , dp);
+        int pick = 0;
+        if(sum >= nums[i]){
+            pick = solve(i-1 , sum - nums[i] , nums , dp);
+        }
+        return dp[i][sum] = pick + notpick;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
+        
+        int sum = 0 ;
+        
+        for(auto x : nums){
+            sum += x;
+        }
+        
+        if(sum - target < 0) return 0;
+        if((sum-target)%2  == 1 ) return 0;
+        int s2 = (sum  - target)/2;
         int n = nums.size();
-        return f(n-1 , 0 , nums , target);
+        vector<vector<int>>dp(n , vector<int>(s2+1 , -1));
+        return solve(n-1 , s2 , nums , dp);
         
     }
 };
