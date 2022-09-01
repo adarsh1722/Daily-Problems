e97f6ffd3dfecd44e5301916b8c538c307e2fcd5
adarsh1722 <adarsh0004singh@gmail.com>
2022-09-02 02:20:07 +0530

    Time: 524 ms (5.07%), Space: 95.6 MB (15.05%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices, int fee) {
-        
-        int n = prices.size();
-        vector<vector<int>>dp(n+1 , vector<int>(2 , -1));
-        dp[n][0] = dp[n][1] = 0;
-        
-        for(int ind = n-1 ; ind >= 0 ; ind--){
-            for(int buy = 0 ; buy <= 1 ; buy++){
-                 long profit = 0;
-                if(buy){
-                    profit = max(-prices[ind] + dp[ind+1][ 0 ], 
-                                0 + dp[ind+1][1]);
-                }
-                else{
-                    profit = max(prices[ind] + dp[ind+1][1] - fee ,
-                                0 + dp[ind+1][0]);
-                }
-                dp[ind][buy] = profit;
-            }
+       int solve(int i , int buy , int n ,int fee , vector<int>&prices , vector<vector<int>>&dp){
+        if(i == n){
+            return 0;
         }
-        return dp[0][1];
+        if(dp[i][buy] != -1) return dp[i][buy];
+        long long int profit = 0;
+        if(buy == 0){// we can buy the stock
+            profit = max(0 + solve(i+1 , 0 , n ,fee , prices , dp) ,  -prices[i] + solve(i+1 , 1 ,  n ,fee , prices , dp));
+        }
+        else{// we can sell the stock
+            profit = max(0 + solve(i+1 , 1 , n ,fee ,prices , dp) , (+prices[i] - fee) + solve(i+1 , 0 , n ,fee , prices , dp));
+        }
+        return dp[i][buy] = profit;
+    }
+    int maxProfit(vector<int>& prices , int fee) {
+        // 0 - buy
+        // 1 - sell
+        int n = prices.size();
+        vector<vector<int>>dp(n ,vector<int>(2 , -1));
+        return solve(0 , 0 , n , fee ,prices , dp);
     }
 };
