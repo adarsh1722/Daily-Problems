2304405d907b49a0eaeb8e5002dbc5152d37205a
adarsh1722 <adarsh0004singh@gmail.com>
2022-09-04 14:07:10 +0530

    Time: 747 ms (41.03%), Space: 10.2 MB (72.60%) - LeetHub

---
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int f(int i , int j , vector<int>&nums , vector<vector<int>>&dp){
+        
+        if(i > j) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        int mini = -1e9;
+        
+        
+        for(int k = i ; k <= j ; k++){
+            
+            int steps = nums[k]*nums[i-1]*nums[j+1] + f(i , k - 1 , nums , dp) + f(k+1 , j , nums , dp);
+            mini = max(mini , steps);
+            
+        }
+        return dp[i][j] = mini;
+        
+    }
+    int maxCoins(vector<int>& nums) {
+        
+        int n = nums.size();
+        nums.push_back(1);
+        nums.insert(nums.begin() , 1);
+        
+        vector<vector<int>>dp(n+1 ,vector<int>(n+1 , -1));
+        return f(1 ,n , nums , dp);
+        
+        
+    }
+};
