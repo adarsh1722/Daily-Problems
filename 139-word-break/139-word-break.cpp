95ef276abd21688d8c0092d2970e6b683ffec7fd
adarsh1722 <adarsh0004singh@gmail.com>
2022-09-06 15:04:48 +0530

    Time: 141 ms (5.01%), Space: 22.8 MB (7.56%) - LeetHub

---
+++ 139-word-break/139-word-break.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    map<string , int>mp;
+    bool solve(int start , int curr , int n , string &s , vector<vector<int>>&dp){
+        
+        if(curr == n) return mp[s.substr(start , curr - start)];
+        
+        if(dp[start][curr] != -1) return dp[start][curr];
+        int nottake = solve(start , curr+1  , n , s , dp);
+        bool check = mp[s.substr(start , curr - start+1 )];
+        int take = check && solve(curr+1 , curr+1 , n , s , dp);
+        
+        return dp[start][curr] =  take || nottake;
+       
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        mp[""] = 1;
+        for(auto  it : wordDict){
+            mp[it] = 1;
+        }
+        int n = s.size();
+        vector<vector<int>>dp(n+1 , vector<int>(n+1,-1));
+        return solve(0 ,0 , n , s , dp);
+    }
+};
