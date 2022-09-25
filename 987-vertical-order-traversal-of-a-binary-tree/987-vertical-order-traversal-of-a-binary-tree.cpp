b45eec5c00a6a8841e52d405b1bfa2ed39f08c9c
adarsh1722 <adarsh0004singh@gmail.com>
2022-09-25 11:07:59 +0530

    Time: 12 ms (32.04%), Space: 13.6 MB (36.71%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,47 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         
-        map<int , map<int , multiset<int>>>nodes;
-        queue<pair<TreeNode* , pair<int , int>>>todo;
-        todo.push({root , {0 , 0}});
-        
-        while(!todo.empty()){
+        vector<vector<int>>ans;
+        // <column , <level , values>>
+        map<int , map<int , multiset<int>>>mp;
+        // <root , <column  , level>>
+        queue<pair<TreeNode* , pair<int ,int>>>q;
+        q.push({root , {0 , 0}});
             
-            auto p = todo.front();
-            todo.pop();
+        while(!q.empty()){
+            
+            auto p = q.front();
+            q.pop();
             TreeNode* node = p.first;
             int x = p.second.first , y = p.second.second;
-
-            // x =>column  , y => level
-            nodes[x][y].insert(node->val);
+            
+            // x => column , y => level
+            mp[x][y].insert(node->val);
             
             if(node->left){
-                todo.push({node->left , {x-1 , y+1}});
+                q.push({node->left , {x-1 , y+1}});
             }
             if(node->right){
-                todo.push({node->right ,{x+1 , y+1}});
+                q.push({node->right , {x+1 , y+1}});
             }
-                       
             
         }
         
-        vector<vector<int>>ans;
-        
-        for(auto p : nodes){
-            
+        for(auto it : mp){
             vector<int>col;
-            for(auto q : p.second){
-              col.insert(col.end() , q.second.begin() , q.second.end());
-
+            for(auto q : it.second){
+                        //(position , start_iterator  ,end_iterator)
+                col.insert(col.end() , q.second.begin() , q.second.end());
             }
             ans.push_back(col);
             
         }
         return ans;
-        
-        
-        
         
     }
 };
