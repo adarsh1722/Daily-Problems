febf24178c0a01eb129674264429e1545c01ddbb
adarsh1722 <adarsh0004singh@gmail.com>
2022-09-25 22:42:33 +0530

    Time: 11 ms (78.09%), Space: 11.4 MB (27.82%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -1,54 +1,44 @@
-
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
 
 class Solution {
 public:
     Node* copyRandomList(Node* head) {
         
-        
-        // nodes ke beech me node dalo
-        Node* node = head;
-        
-        while(node){
-            Node* temp = node->next;
-            node->next = new Node(node->val);
-            node->next->next = temp;
-            node = temp;
-        }
-        
-        // random pointer ko copy krna h
-        
-        node = head;
-        
-        while(node){
-            
-            if(node->random){
-                
-                node->next->random = node->random->next;
-            }
-            
-            node = node->next->next;
-            
-            
+        map<Node* , Node*>mp;
+        Node* temp = head;
+        Node* dummy = new Node(-1);
+        Node* tail = dummy;
+        while(temp){
+            Node* now  = new Node(temp->val);
+            tail->next = now;
+            tail = tail->next;
+            mp[temp] = now;
+            temp = temp->next;
             
         }
         
-        // node ko alag kr lo
-        
-        Node* ans  = new Node(0);
-        Node* helper = ans;
-        while(head){
-            
-            helper->next = head->next;
-            helper = helper->next;
-            
-            
-            head->next = head->next->next;
-            head = head->next;
-            
+        temp = head;
+        while(temp){
+            Node * newRandom = temp->random;
+            mp[temp]->random = mp[newRandom];
+            temp = temp->next;
         }
         
-        return ans->next;
-        
+        return dummy->next;
         
         
     }
