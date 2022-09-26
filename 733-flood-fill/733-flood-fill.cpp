a951057abae7cc9baff80e81c2a7bbdfcca0cdc5
adarsh1722 <adarsh0004singh@gmail.com>
2022-09-26 22:27:11 +0530

    Time: 13 ms (68.78%), Space: 13.9 MB (78.76%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    void dfs(int sr , int sc , int parent  , int n , int m ,vector<vector<int>>& image , int color){
+    void dfs(int sr , int sc ,int color , int parent , vector<vector<int>>&image){
         
-        // invalid case
-        if(sr < 0 || sc < 0 || sr >= n || sc >= m || image[sr][sc] != parent) return ;
+        int n = image.size() , m = image[0].size();
+        // base check
+        if(sr < 0 || sc < 0 || sr >= n || sc >= m || image[sr][sc] != parent){
+            return;
+        }
         
-        // make change
+        // make the change
         
         image[sr][sc] = color;
         
-        // do dfs in 4- direction
-        dfs(sr+1 , sc  , parent , n , m , image , color);
-        dfs(sr-1 , sc , parent , n , m ,image , color);
-        dfs(sr , sc-1 , parent , n ,  m ,image , color);
-        dfs(sr , sc+1 ,parent , n , m ,image ,color);
-        
-        
+        dfs(sr+1 , sc , color, parent , image);
+        dfs(sr-1 , sc , color, parent , image);
+        dfs(sr , sc+1 , color, parent , image);
+        dfs(sr , sc-1 , color, parent , image);
         
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         
+        int n =  image.size() , m = image[0].size();
+        
         if(image[sr][sc] == color) return image;
-        int n = image.size() , m = image[0].size();
-        dfs(sr , sc , image[sr][sc] , n , m ,image , color);
+        
+        dfs(sr , sc , color, image[sr][sc] , image);
         return image;
         
     }
