dd9fbd4696e9be39230b0f023e0664c46f3df136
adarsh1722 <adarsh0004singh@gmail.com>
2022-09-26 10:40:43 +0530

    Time: 27 ms (37.36%), Space: 16 MB (18.13%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -2,47 +2,45 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         
-        int cand1 = 0 , cand2 = 0 , count1 = 0  , count2 = 0;
         int n = nums.size();
-        for(int i = 0 ; i < n ; i++)
-        {
-            if(nums[i] == cand1)
-            {
+        
+        int count1 = 0 , count2 = 0  , num1 = 0  , num2 = 0 ;
+        
+        for(int i = 0 ; i < n ; i++){
+            if (num1 == nums[i]){
                 count1++;
             }
-            else if(nums[i] == cand2)
-            {
+            else if(num2 == nums[i]){
                 count2++;
             }
-            else if(count1 == 0)
-            {
-                cand1 = nums[i];
-                count1++;
+            else if(count1 == 0){
+                count1 =  1;
+                num1 = nums[i];
             }
-            else if(count2 == 0)
-            {
-                cand2 = nums[i];
-                count2++;
-                
+            else if(count2 == 0){
+                count2 = 1;
+                num2 = nums[i];
             }
+           
             else{
-                 count1--;
-                 count2--;
+                count1--;
+                count2--;
             }
+            
             
         }
         
-        vector< int > ans;
-        
-        count1 =  count2 = 0;
-        
-        for(int i = 0 ; i < n ; i++)
-        {
-            if(nums[i] == cand1)count1++;
-            else if(nums[i] == cand2) count2++;
+        count1 = count2 = 0;
+        for(int i = 0 ; i < n; i++){
+            if(nums[i] == num1) count1++;
+            else if(nums[i] == num2) count2++;
         }
-        if(count1 > n/3) ans.push_back(cand1);
-        if(count2 > n/3) ans.push_back(cand2);
+        
+        vector<int>ans;
+        if(count1 > n/3) ans.push_back(num1);
+        if(count2 > n/3) ans.push_back(num2);
+        
         return ans;
+        
     }
 };
