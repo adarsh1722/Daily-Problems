c9a247a6657e3b8b5b315327eddcb0a920c2152e
adarsh1722 <adarsh0004singh@gmail.com>
2022-09-29 11:19:47 +0530

    Time: 182 ms (48.11%), Space: 8.2 MB (64.66%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -5,51 +5,54 @@ public:
         int n = s.size();
         bool table[n][n];
         
-        memset(table , 0 , sizeof table);
+        memset(table , false , sizeof table);
         
         int maxLength = 1;
         
-        // one length ki substring ko mark kr do true
+        // check of size 1
         for(int i = 0 ; i < n ; i++){
             table[i][i] = true;
         }
         
         int start = 0;
         
-        // 2 length ki subtring ko mark kr d0
-        for(int i = 0 ; i < n-1 ; i++){
+        // check for size 2
+        for(int i = 0 ; i <  n-1 ; i++){
             if(s[i] == s[i+1]){
                 table[i][i+1] = true;
                 start = i;
                 maxLength = 2;
             }
         }
-        // hr length ki try kro
         
-        for(int k = 3 ; k <=  n ; k++){
-            for(int i = 0 ; i < n-k+1 ; i++){
+        // check for every size of k from 3 to n
+        for(int k = 3 ; k <= n ; k++){
+            for(int i = 0 ; i <= n-k ; i++){
                 
                 int j = i + k-1;
                 
-                if(table[i+1][j-1] == true && s[i] == s[j]){
+                if(table[i+1][j-1] && s[i] == s[j]){
                     table[i][j] = true;
+                    
                     if(k > maxLength){
                         start = i;
                         maxLength = k;
                     }
                 }
-                
             }
+            
         }
         
         string str = "";
         
-        int i = start;
+        int i  = start;
         
         for(int k = 0 ; k < maxLength ; k++){
-            str += s[i++];
+            str += s[i+k];
         }
+        
         return str;
+        
         
         
     }
